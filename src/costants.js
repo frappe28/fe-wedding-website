78a443e2f6979c923160d28b15de11894ec5bb98
Francesca Deperte <francescadeperte@gmail.com>
2024-12-08 23:23:27 +0100

    agginto url dinamico in base all'ambiente

--- src/costants.js
+++ src/costants.js
@@ -1,4 +1,4 @@
-export const API_GW_URL = 'https://1hqoh8g3ob.execute-api.eu-west-1.amazonaws.com/dev'
-export const BASE_URL = 'https://d25ynqiopfm3p.cloudfront.net'
-export const THE_DATE = '2025-05-14T00:00:00'
-export const DATE_AFTER_XMAS = '2024-12-01T00:00:00'
+export const API_GW_URL = process.env.ENV === 'dev' ? 'https://1hqoh8g3ob.execute-api.eu-west-1.amazonaws.com/dev' : 'https://qrg1axgo78.execute-api.eu-west-1.amazonaws.com/prod';
+export const BASE_URL = process.env.ENV === 'dev' ? 'https://d25ynqiopfm3p.cloudfront.net' : 'https://d3ir72yyqqvtcb.cloudfront.net';
+export const THE_DATE = '2025-05-14T00:00:00';
+export const DATE_AFTER_XMAS = '2025-01-10T00:00:00';
