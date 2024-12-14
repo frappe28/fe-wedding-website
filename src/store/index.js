d63e49ad240bdcd84a72612cdb0f34d666513aea
m.cetronio <m.cetronio@reply.it>
2024-12-15 00:35:40 +0100

    Revert "eliminazione file ritenuti inutili"
    This reverts commit a0f89f9ee8d68ada024ceeed494fb9475d1c2179.

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+import user from './user.js';
+
+const store = createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    user
+  }
+});
+
+export default store;
